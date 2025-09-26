4fc6428f72ec41eed1866cd0b6af457c702474aa
Anh Khoi NGUYEN HO <anhkhoi.nguyenho@edu.esiee.fr>
2025-09-26 07:48:55 +0000

    pylint score 8.33, à améliorer : ajout de docstring

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from math import sqrt
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    assert p >= 0, "Le nombre doit être positif"
+    premier = True
+    if p < 2:
+        premier = False
+    # La boucle ne tournera pas pour p = 2 et p = 3  car
+    # sqrt(2) < 2
+    # sqrt(3) < 2
+    for d in range(2, int(sqrt(p)) + 1): # exclure 0 et 1 pour exclure p = p x 1
+        premier = bool(p%d)
+        if not premier:
+            break
+    return premier
 
 #### Fonction principale
 
